10176515132db896a09c950e1761dd59e168ba0c
ppmblue <nhu1142002@gmail.com>
2024-03-11 22:28:43 +0700

    add cors domain

--- LMSSystemBackend/settings.py
+++ LMSSystemBackend/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "LMSSystemBackend.urls"
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ("http://localhost:8081",)
+CORS_ORIGIN_WHITELIST = ("http://localhost:8081", "http://103.75.186.201:8081")
 
 TEMPLATES = [
     {
